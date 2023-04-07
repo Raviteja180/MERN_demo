fcdbdc7f65de2b2e888209fb3b48433fcd2a87cc
Raviteja180 <tatipamularaviteja525@gmail.com>
2023-04-07 17:30:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
+import axios from 'axios';
 function App() {
+  const [name,setName]= useState("");
+  const [password,setPassword]=useState("");
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    axios.post('http://localhost:4000/users',{"name":name,"password":password})
+    .then((res)=>console.log(res));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>Name :</label>
+      <input type="text"  onChange={(e)=>setName(e.target.value)}/>
+      <label>Passoword</label>
+      <input type="text" onChange={(e)=>setPassword(e.target.value)}/>
+      <button>Submit</button>
+    </form>
   );
 }
 
